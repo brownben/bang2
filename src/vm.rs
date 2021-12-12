6424922a082cc5e361bfb9505c99e79429acfc42
brownben <9870007+brownben@users.noreply.github.com>
2021-12-12 12:57:49 +0000

    refactor: move chunk to local

--- src/vm.rs
+++ src/vm.rs
@@ -5,55 +5,63 @@ use crate::value::Value;
 use std::collections::HashMap;
 use std::rc::Rc;
 
-macro_rules! get_safe {
-  ($vm:expr, $x:expr , $ip:expr ) => {
-    match $x {
+macro_rules! get {
+  ($vm:expr, $value:expr, $chunk:expr, $ip:expr) => {
+    match $value {
       Some(value) => value,
-      None => break $vm.runtime_error("Problem accessing stack value", $ip),
+      None => break runtime_error!($vm, "Problem accessing stack value", $chunk, $ip),
+    }
+  };
+
+  (Pop $vm:expr, $chunk:expr, $ip:expr) => {
+    get!($vm, $vm.stack.pop(), $chunk, $ip)
+  };
+
+  (Constant $vm:expr, $location:expr, $chunk:expr, $ip:expr) => {
+    get!($vm, $chunk.get_constant($location as usize), $chunk, $ip)
+  };
+
+  (Value $vm:expr, $chunk:expr, $ip:expr) => {
+    get!($vm, $chunk.get_value($ip), $chunk, $ip)
+  };
+
+  (Long $vm:expr, $chunk:expr, $ip:expr) => {
+    get!($vm, $chunk.get_long_value($ip), $chunk, $ip)
+  };
+
+  (Two $vm:expr, $chunk:expr, $ip:expr) => {
+    (
+      get!(Pop $vm, $chunk, $ip),
+      get!(Pop $vm, $chunk, $ip),
+    )
+  };
+}
+
+macro_rules! runtime_error {
+  ($vm:expr, $message:expr, $chunk:expr, $ip:expr) => {
+    {
+      let line_number = $chunk.get_line_number($ip);
+
+      $vm.stack.clear();
+
+      Err(RuntimeError {
+        message: $message.to_string(),
+        line_number,
+      })
     }
   };
 }
 
-macro_rules! get_two_values {
-  ($vm:expr,  $ip:expr ) => {
-    (get_pop!($vm, $ip), get_pop!($vm, $ip))
-  };
-}
-
-macro_rules! get_long_value {
-  ( $vm:expr, $ip:expr ) => {
-    get_safe!($vm, $vm.chunk.get_long_value($ip), $ip)
-  };
-}
-
-macro_rules! get_value {
-  ( $vm:expr, $ip:expr ) => {
-    get_safe!($vm, $vm.chunk.get_value($ip), $ip)
-  };
-}
-
-macro_rules! get_constant {
-  ( $vm:expr, $location:expr, $ip:expr ) => {
-    get_safe!($vm, $vm.chunk.get_constant($location as usize), $ip)
-  };
-}
-
-macro_rules! get_pop {
-  ( $vm:expr, $ip:expr ) => {
-    get_safe!($vm, $vm.stack.pop(), $ip)
-  };
-}
-
 macro_rules! numeric_expression {
-  ( $vm:expr, $token:tt, $ip:expr ) => {
-    numeric_expression!($vm, $token, Number, $ip)
+  ($vm:expr, $token:tt, $chunk:expr, $ip:expr) => {
+    numeric_expression!($vm, $token, Number, $chunk, $ip)
   };
 
-  ( $vm:expr, $token:tt, $type:tt, $ip:expr ) => {
-    let (right, left) = get_two_values!($vm, $ip);
+  ($vm:expr, $token:tt, $type:tt, $chunk:expr, $ip:expr) => {
+    let (right, left) = get!(Two $vm, $chunk, $ip);
 
     if !left.is_number() || !right.is_number() {
-      break $vm.runtime_error("Both operands must be numbers.", $ip);
+      break runtime_error!($vm, "Both operands must be numbers.", $chunk, $ip);
     }
 
     $vm.stack.push(
@@ -69,13 +77,11 @@ macro_rules! numeric_expression {
 struct VM {
   stack: Vec<Value>,
   pub globals: HashMap<Rc<str>, Value>,
-  chunk: Chunk,
 }
 
 impl VM {
-  fn new(chunk: Chunk) -> Self {
+  fn new() -> Self {
     Self {
-      chunk,
       stack: Vec::new(),
       globals: HashMap::new(),
     }
@@ -89,22 +95,22 @@ impl VM {
     self.stack.get(self.stack.len() - 2).unwrap()
   }
 
-  fn run(&mut self) -> Result<(), RuntimeError> {
+  fn run(&mut self, chunk: Chunk) -> Result<(), RuntimeError> {
     let mut ip = 0;
 
     loop {
-      let instruction = self.chunk.get(ip);
+      let instruction = chunk.get(ip);
 
       match instruction {
         Some(OpCode::Constant) => {
-          let constant_location = get_value!(self, ip + 1);
-          let constant = get_constant!(self, constant_location, ip);
+          let constant_location = get!(Value self, chunk, ip + 1);
+          let constant = get!(Constant self, constant_location, chunk, ip);
           self.stack.push(constant);
           ip += 2;
         }
         Some(OpCode::ConstantLong) => {
-          let constant_location = get_long_value!(self, ip + 1) as u16;
-          let constant = get_constant!(self, constant_location, ip);
+          let constant_location = get!(Long self, chunk, ip + 1) as u16;
+          let constant = get!(Constant self, constant_location, chunk, ip);
           self.stack.push(constant);
           ip += 3;
         }
@@ -123,66 +129,71 @@ impl VM {
         Some(OpCode::Add) => {
           let first_operand = self.peek_2();
           if first_operand.is_string() {
-            let (right, left) = get_two_values!(self, ip);
+            let (right, left) = get!(Two self, chunk, ip);
 
             if !left.is_string() || !right.is_string() {
-              break self.runtime_error("Both operands must be strings.", ip);
+              break runtime_error!(self, "Both operands must be strings.", chunk, ip);
             }
 
             let concatenated = format!("{}{}", left.get_string_value(), right.get_string_value());
             self.stack.push(Value::from(concatenated));
           } else if first_operand.is_number() {
-            numeric_expression!(self, +, ip);
+            numeric_expression!(self, +, chunk, ip);
           } else {
-            break self.runtime_error("Operands must be two numbers or two strings.", ip);
+            break runtime_error!(
+              self,
+              "Operands must be two numbers or two strings.",
+              chunk,
+              ip
+            );
           }
 
           ip += 1;
         }
         Some(OpCode::Subtract) => {
-          numeric_expression!(self, -, ip);
+          numeric_expression!(self, -, chunk, ip);
           ip += 1;
         }
         Some(OpCode::Multiply) => {
-          numeric_expression!(self, *, ip);
+          numeric_expression!(self, *, chunk, ip);
           ip += 1;
         }
         Some(OpCode::Divide) => {
-          numeric_expression!(self, /, ip);
+          numeric_expression!(self, /, chunk, ip);
           ip += 1;
         }
         Some(OpCode::Negate) => {
-          let value = get_pop!(self, ip);
-          match value {
-            Value::Number(n) => self.stack.push(Value::from(-n)),
-            _ => {
-              break self.runtime_error("Operand must be a number.", ip);
-            }
+          let value = get!(Pop self, chunk, ip);
+          if let Value::Number(n) = value {
+            self.stack.push(Value::from(-n))
+          } else {
+            break runtime_error!(self, "Operand must be a number.", chunk, ip);
           }
+
           ip += 1;
         }
         Some(OpCode::Not) => {
-          let value = get_pop!(self, ip);
+          let value = get!(Pop self, chunk, ip);
           self.stack.push(Value::from(value.is_falsy()));
           ip += 1;
         }
 
         Some(OpCode::Equal) => {
-          let (right, left) = get_two_values!(self, ip);
+          let (right, left) = get!(Two self, chunk, ip);
           self.stack.push(Value::from(left.equals(&right)));
           ip += 1;
         }
         Some(OpCode::Less) => {
-          numeric_expression!(self, <, Boolean, ip);
+          numeric_expression!(self, <, Boolean, chunk, ip);
           ip += 1;
         }
         Some(OpCode::Greater) => {
-          numeric_expression!(self, >, Boolean, ip);
+          numeric_expression!(self, >, Boolean, chunk, ip);
           ip += 1;
         }
 
         Some(OpCode::Print) => {
-          let value = get_pop!(self, ip);
+          let value = get!(Pop self, chunk, ip);
           println!("{}", value);
           ip += 1;
         }
@@ -192,17 +203,17 @@ impl VM {
         }
 
         Some(OpCode::DefineGlobal) => {
-          let name_location = get_value!(self, ip + 1);
-          let name = get_constant!(self, name_location, ip);
+          let name_location = get!(Value self, chunk, ip + 1);
+          let name = get!(Constant self, name_location, chunk, ip);
 
-          let value = get_pop!(self, ip);
+          let value = get!(Pop self, chunk, ip);
           self.globals.insert(name.get_string_value(), value);
 
           ip += 2;
         }
         Some(OpCode::GetGlobal) => {
-          let name_location = get_value!(self, ip + 1);
-          let name = get_constant!(self, name_location, ip);
+          let name_location = get!(Value self, chunk, ip + 1);
+          let name = get!(Constant self, name_location, chunk, ip);
 
           let value = self.globals.get(&name.get_string_value());
 
@@ -210,14 +221,14 @@ impl VM {
             self.stack.push(value.clone());
           } else {
             let message = format!("Undefined variable '{}'", name.get_string_value());
-            break self.runtime_error(&message, ip);
+            break runtime_error!(self, &message, chunk, ip);
           }
 
           ip += 2;
         }
         Some(OpCode::SetGlobal) => {
-          let name_location = get_value!(self, ip + 1);
-          let name = get_constant!(self, name_location, ip);
+          let name_location = get!(Value self, chunk, ip + 1);
+          let name = get!(Constant self, name_location, chunk, ip);
           let value = self.peek().clone();
 
           if let std::collections::hash_map::Entry::Occupied(mut entry) =
@@ -226,24 +237,24 @@ impl VM {
             entry.insert(value);
           } else {
             let message = format!("Undefined variable '{}'", name.get_string_value());
-            break self.runtime_error(&message, ip);
+            break runtime_error!(self, &message, chunk, ip);
           }
 
           ip += 2;
         }
         Some(OpCode::GetLocal) => {
-          let slot = get_value!(self, ip + 1);
+          let slot = get!(Value self, chunk, ip + 1);
           self.stack.push(self.stack[slot as usize].clone());
           ip += 2;
         }
         Some(OpCode::SetLocal) => {
-          let slot = get_value!(self, ip + 1);
+          let slot = get!(Value self, chunk, ip + 1);
           self.stack[slot as usize] = self.peek().clone();
           ip += 2;
         }
 
         Some(OpCode::JumpIfFalse) => {
-          let offset = get_long_value!(self, ip + 1);
+          let offset = get!(Long self, chunk, ip + 1);
           if self.peek().is_falsy() {
             ip += offset as usize + 1;
           } else {
@@ -251,7 +262,7 @@ impl VM {
           }
         }
         Some(OpCode::JumpIfNull) => {
-          let offset = get_long_value!(self, ip + 1);
+          let offset = get!(Long self, chunk, ip + 1);
           if self.peek().is_null() {
             ip += offset as usize + 1;
           } else {
@@ -259,44 +270,33 @@ impl VM {
           }
         }
         Some(OpCode::Jump) => {
-          let offset = get_long_value!(self, ip + 1);
+          let offset = get!(Long self, chunk, ip + 1);
           ip += offset as usize + 1;
         }
         Some(OpCode::Loop) => {
-          let offset = get_long_value!(self, ip + 1);
+          let offset = get!(Long self, chunk, ip + 1);
           ip -= offset as usize - 1;
         }
 
         Some(OpCode::Return) => break Ok(()),
         None => {
-          break self.runtime_error("Unknown OpCode", ip);
+          break runtime_error!(self, "Unknown OpCode", chunk, ip);
         }
       }
 
       #[cfg(feature = "debug-stack")]
       println!("Stack={:?}", self.stack);
 
-      if ip >= self.chunk.length() {
+      if ip >= chunk.length() {
         break Ok(());
       }
     }
   }
-
-  fn runtime_error(&mut self, message: &str, ip: usize) -> Result<(), RuntimeError> {
-    let line_number = self.chunk.get_line_number(ip);
-
-    self.stack.clear();
-
-    Err(RuntimeError {
-      message: message.to_string(),
-      line_number,
-    })
-  }
 }
 
 pub fn run(chunk: Chunk) -> Result<HashMap<Rc<str>, Value>, RuntimeError> {
-  let mut vm = VM::new(chunk);
-  vm.run()?;
+  let mut vm = VM::new();
+  vm.run(chunk)?;
 
   Ok(vm.globals)
 }
