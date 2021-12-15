3064b7775c9d2af2f2b79bd6941f61bec21b64e0
brownben <9870007+brownben@users.noreply.github.com>
2021-12-15 23:59:02 +0000

    test: lint rules

---
+++ tests/linter.rs
@@ -0,0 +1,110 @@
+use bang;
+
+macro_rules! bang_lint {
+  ($name:ident $code:literal $($rule:literal [$($num:literal)*])*) => {
+    #[test]
+    fn $name() {
+      match bang::parse($code) {
+        Ok(ast) => {
+          let warnings = bang::lint(&ast);
+          $({
+            let warning = warnings.iter().find(|warning| warning.name == $rule).unwrap();
+            assert_eq!(warning.issues.iter().map(|token| token.line).collect::<Vec<u16>>(), vec![$($num),*]);
+          };)*
+        }
+        Err(_) => assert!(false, "Failed to parse code"),
+      }
+    }
+  };
+}
+
+bang_lint!(no_constant_condition
+"
+if (true)
+  do_stuff()
+else
+  do_stuff()
+
+if (x > 6)
+  do_stuff()
+else if (false)
+  do_stuff()
+
+while (4 > 5)
+  do_stuff()
+
+while (question() > 4)
+  do_stuff()
+"
+  "No Constant Conditions" [2 9 12]
+);
+
+bang_lint!(no_negative_zero
+"
+-0
+0
+--0
+-0.0
+"
+  "No Negative Zero" [2 4 5]
+);
+
+bang_lint!(no_self_assign
+"
+let a = 8
+  let a = a
+  a += 1
+
+let b = 0
+b = b
+"
+  "No Self Assign" [7]
+);
+
+bang_lint!(no_unreachable_code
+"
+fun x()
+  if (true)
+    return 10
+    type(\"Hello\")
+  else
+    return 5
+
+fun y()
+  if (true)
+    return 10
+  x()
+  return
+  77
+  null
+"
+  "No Unreachable Code" [4 13]
+);
+
+bang_lint!(no_yoda_equality_check
+"
+x == y
+x == 7
+7 == x
+x != y
+x != 7
+7 != x
+7 < x
+x > 7
+"
+  "No Yoda Equality" [4 7]
+);
+
+bang_lint!(no_constant_condition_and_unreachable_code
+"
+if (true)
+  do_stuff()
+  return 10
+  do_other_stuff()
+else
+  do_stuff()
+
+"
+  "No Constant Conditions" [2]
+  "No Unreachable Code" [4]
+);
